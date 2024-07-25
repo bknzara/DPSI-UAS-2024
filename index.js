7b35697bdedc736744602f57339d1a84bfc1023e
bknzara <zhrputriaz@gmail.com>
2024-07-25 09:31:14 +0700

    memperbaiki

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const sequelize = require('./models/index');
+
+const mahasiswaRouter = require('./routes/mahasiswa');
+const pesanRouter = require('./routes/pesan');
+const informasiAkademikRouter = require('./routes/informasi_akademik');
+const dpaRouter = require('./routes/dpa');
+const formulirPerwalianRouter = require('./routes/formulir_perwalian');
+const jadwalPerwalianRouter = require('./routes/jadwal_perwalian');
+const authRouter = require('./routes/user');
+
+// Middleware untuk parsing JSON
+app.use(bodyParser.json());
+
+app.use('/mahasiswa', mahasiswaRouter);
+app.use('/pesan', pesanRouter);
+app.use('/informasi-akademik', informasiAkademikRouter);
+app.use('/dpa', dpaRouter);
+app.use('/formulir-perwalian', formulirPerwalianRouter);
+app.use('/jadwal-perwalian', jadwalPerwalianRouter);
+app.use('/auth', authRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
