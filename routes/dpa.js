c6a005d3f211550a514804dbfc3dc1f2d88d3ffe
bknzara <129248999+bknzara@users.noreply.github.com>
2024-07-25 18:01:30 +0700

    Update dpa.js
    memberikan komentar

--- routes/dpa.js
+++ routes/dpa.js
@@ -30,6 +30,16 @@ router.post('/', authenticate, authorize(['dpa']), async (req, res) => {
   }
 });
 
+//authenticate ini adalah middleware yang biasanya digunakan untuk memverifikasi identitas pengguna yang membuat permintaan. 
+//Middleware ini akan memeriksa apakah permintaan datang dari pengguna yang terautentikasi (misalnya, dengan memeriksa token otentikasi dalam header permintaan).
+//Jika autentikasi gagal, permintaan akan dihentikan dan pengguna akan menerima respons kesalahan.
+//authorize(['dpa']) Ini adalah middleware lain yang digunakan untuk memeriksa apakah pengguna yang telah diautentikasi memiliki izin yang sesuai untuk mengakses sumber daya. 
+//Dalam hal ini, middleware ini memeriksa apakah pengguna memiliki hak akses yang terkait dengan peran 'dpa'. 
+//Biasanya, fungsi authorize akan memeriksa peran atau hak akses pengguna yang sudah diautentikasi dan membandingkannya dengan hak akses yang diperlukan untuk melakukan tindakan ini. 
+//Jika pengguna tidak memiliki izin yang diperlukan, permintaan akan dihentikan, dan pengguna akan menerima respons kesalahan.
+
+authorize(['dpa']): Ini adalah middleware lain yang digunakan untuk memeriksa apakah pengguna yang telah diautentikasi memiliki izin yang sesuai untuk mengakses sumber daya. Dalam hal ini, middleware ini memeriksa apakah pengguna memiliki hak akses yang terkait dengan peran 'dpa'. Biasanya, fungsi authorize akan memeriksa peran atau hak akses pengguna yang sudah diautentikasi dan membandingkannya dengan hak akses yang diperlukan untuk melakukan tindakan ini. Jika pengguna tidak memiliki izin yang diperlukan, permintaan akan dihentikan, dan pengguna akan menerima respons kesalahan.
+
 // Read all
 router.get('/', authenticate, authorize(['dpa']), async (req, res) => {
   try {
